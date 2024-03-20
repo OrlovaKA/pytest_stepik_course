287ba8babe16925786acf1bebd44d067efce9a2b
OrlovaKA <154547831+OrlovaKA@users.noreply.github.com>
2024-03-20 20:24:40 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+from selenium.webdriver.common.by import By
+import time
 
 def test_add_to_cart_button_is_displayed(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    print("\nstart browser for test..")
     browser.get(link)
-    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket") 
     assert button, 'Button not found!'
